509e0cab455daaa7a075a0eee6cce26e19194058
DimitrivC <d.j.vancapelleveen@xs4all.nl>
2017-11-24 12:55:39 +0100

    done!

--- app/src/main/java/com/example/dimitrivc/todolist/TodoAdapter.java
+++ app/src/main/java/com/example/dimitrivc/todolist/TodoAdapter.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 
@@ -27,36 +29,29 @@ public class TodoAdapter extends ResourceCursorAdapter{
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-            // this method fills the right elements with data from the cursor.
-            // lijkt me dat de controls, de elements (zoals textview) gevuld moeten worden met data van de cursor
 
         TextView todoItem = view.findViewById(R.id.textViewTodoItem);
+        TextView completedview = view.findViewById(R.id.textViewCompleted);
 
-        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-            String data = cursor.getString(cursor.getColumnIndex("Dishes"));
-            todoItem.setText(data);
-        }
+       // if (cursor.moveToFirst()){
+         //   while (!cursor.isAfterLast()){
 
-        //if (cursor) {
+                Integer name = cursor.getColumnIndex("title");
+                String name2 = cursor.getString(name);
+                //Log.d("taart", name2);
+                todoItem.setText(name2);
 
-//            TextView todoItem = view.findViewById(R.id.textViewTodoItem);
-//            Integer name = cursor.getColumnIndex("Dishes");
-//            //String column_value = cursor.getString(name);
-//            // gets the column index for a column named name:
-//            todoItem.setText(name.toString());
-//            // werkt mogelijk niet goed.
+                Integer name3 = cursor.getColumnIndex("completed");
+                Integer name4 = cursor.getInt(name3);
+                //Log.d("taart1", String.valueOf(name4));
 
-            TextView completedview = view.findViewById(R.id.textViewCompleted);
-            // retrieves the value of one column as an int (for completeness I guess).
-            Integer completeness_int = cursor.getInt(2);
-            if (completeness_int == 0)
-                completedview.setText("completed");
-            else
-                completedview.setText(completeness_int.toString());
-        //}
+                //CheckBox completedness = view.findViewById(R.id.checkBox);
 
-        // miss:
-        //cursor.close();
+                completedview.setText(String.valueOf(name4));
+
+            //    cursor.moveToNext();
+          //  }
+       // }
 
     // EINDE BINDVIEW
     }
--- app/src/main/java/com/example/dimitrivc/todolist/TodoDatabase.java
+++ app/src/main/java/com/example/dimitrivc/todolist/TodoDatabase.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by DimitrivC on 20-11-2017.
@@ -25,23 +26,24 @@ public class TodoDatabase extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
         sqLiteDatabase.execSQL("CREATE TABLE todos (_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed INT);");
+        sqLiteDatabase.execSQL("INSERT INTO todos (title, completed) VALUES('Trash', 0);");
         sqLiteDatabase.execSQL("INSERT INTO todos (title, completed) VALUES('Laundry', 0);");
         sqLiteDatabase.execSQL("INSERT INTO todos (title, completed) VALUES('Dishes', 0);");
-        sqLiteDatabase.execSQL("INSERT INTO todos (title, completed) VALUES('Trash', 0);");
+
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         // hoezo int i en int il?
-        String TABLE_NAME = "table";
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS" + TABLE_NAME);
+        String TABLE_NAME = "todos";
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 
     public static TodoDatabase getInstance(Context context){
             if (instance == null){
-                instance = new TodoDatabase(context, "table", null, 1 );
+                instance = new TodoDatabase(context, "table", null, 5 );
             }
             return instance;
     }
@@ -50,51 +52,76 @@ public class TodoDatabase extends SQLiteOpenHelper {
         // use getWritableDatabase() to open connection
         SQLiteDatabase db = this.getWritableDatabase();
         String query = "SELECT * FROM todos";
-        Cursor cursor = db.rawQuery(query, null );
+        Cursor cursor = db.rawQuery(query, new String[] {});
             // ipv null iets van: new String[] {String.valueOf(stdId)
 
         return cursor;
     }
 
-//    public void insert(String title, Integer completed) {
-//
-//        // get connection to database: dit op te lossen door static weg te halen, maar dan doet insert
-//        // in main -> addItem het niet meer.
-//        SQLiteDatabase db = getWritableDatabase();
-//
-//        // create new contentvalues object
-//        ContentValues contentValues = new ContentValues();
-//
-//        // use put method to add values for title and completed
-//        contentValues.put(title, completed);
-//
-//        // Then, call insert on the database connection, passing in the right parameters
-//        // weet niet of "table" klopt
-//        // en huh: insert in zichzelf oproepen? al heeft het andere parameters, dus miss zit het wel goed.
-//        db.insert("table", null, contentValues);
-//    }
+    public void insert(String title, Integer completed) {
 
-//    public void update(Long id, Boolean completed) {
-//
-//        // get a reference to the writable database.
-//        SQLiteDatabase db = getWritableDatabase();
-//
-//        // Create a ContentValues object that contains the new value for completed.
-//        ContentValues contentValues = new ContentValues();
-//        String boolean_key = id.toString();
-//        contentValues.put(boolean_key, completed);
-//
-//        // Then call update() on the database.
-//        // Tip: use the whereClause to specify which record you want to update: "_id = " + id.
-//        db.update("table", contentValues, "_id = " + id, null);
-//
-//        // table: String / values: contentValues / whereClause: string / whereArgs: string
-//    }
-//
-//    public void delete(Long id) {
-//        SQLiteDatabase db = getWritableDatabase();
-//        db.delete("table", null, null);
-//        // mogelijke error: die id moet je miss gebruiken om aan te geven wat je wil deleten?
-//        // table: string / whereClause: string / whereArgs: string
-//    }
+        // get connection to database: dit op te lossen door static weg te halen, maar dan doet insert
+        // in main -> addItem het niet meer.
+        SQLiteDatabase db = getWritableDatabase();
+
+        // create new contentvalues object
+        ContentValues contentValues = new ContentValues();
+
+        // use put method to add values for title and completed
+
+        // maar, ipv title moet het
+        contentValues.put("title", title);
+        //contentValues.put("completed", completed);
+
+        // Then, call insert on the database connection, passing in the right parameters
+        // weet niet of "table" klopt
+        // en huh: insert in zichzelf oproepen? al heeft het andere parameters, dus miss zit het wel goed.
+        db.insert("todos", null, contentValues);
+    }
+
+
+
+    public void update(Cursor cursor) {
+
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        //cursor.moveToFirst();
+        int test = cursor.getInt(2);
+
+
+
+        if (test == 1){
+            test = 0;
+            //Log.d("test1", String.valueOf(test));
+        }
+        else if (test == 0){
+            test = 1;
+        }
+
+        //test = 0;
+
+        Log.d("test3", String.valueOf(test));
+
+
+        // Create a ContentValues object that contains the new value for completed.
+        ContentValues contentValues = new ContentValues();
+        //String key = id.toString();
+        contentValues.put("completed", test);
+
+
+        Long id2 = cursor.getLong(0);
+        // id ...
+
+        db.update("todos", contentValues, "_id = " + id2, null);
+
+        // table: String / values: contentValues / whereClause: string / whereArgs: string
+    }
+
+    public void delete(Long id) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete("todos", "_id = " + id, null);
+        // mogelijke error: die id moet je miss gebruiken om aan te geven wat je wil deleten?
+        // table: string / whereClause: string / whereArgs: string
+    }
 }
